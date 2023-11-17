811d6a139164aee6ea8de928a71b363290971940
michael-ono <mikeonochie@outlook.com>
2023-11-17 23:54:58 +0100

    navbar-setup

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,11 +7,25 @@ const Navbar = () => {
   return (
     <div className="nav-container">
         <div className="logo-container">
-            <Avatar src={icon} size="large" />
+            <Avatar src={icon} size="large" style={{height: '150px', width: '150px' ,backgroundColor: 'black'}}/>
             <Typography.Title level={2} className="logo">
                 <Link to="/">Cryptoland</Link>
             </Typography.Title>
         </div>
+        <Menu theme="dark">
+            <Menu.Item icon={<HomeOutlined />} >
+                <Link to="/">Home</Link>
+            </Menu.Item>    
+            <Menu.Item icon={<HomeOutlined />} >
+                <Link to="/cryptocurrencies">Cryptocurrencies</Link>
+            </Menu.Item>    
+            <Menu.Item icon={<HomeOutlined />} >
+                <Link to="/exchanges">Exchanges</Link>
+            </Menu.Item>    
+            <Menu.Item icon={<HomeOutlined />} >
+                <Link to="/news">News</Link>
+            </Menu.Item>    
+        </Menu>
     </div>
   )
 }
